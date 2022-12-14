bac720f713195a1cf721328daabaa78b4097f405
hungnguyen50497 <nguyenhung50497@gmail.com>
2022-12-14 11:19:24 +0700

    new

---
+++ .history/BT_xayDungUngDungMayTinh/server_20221214111313.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+const qs = require('qs')
+
+const server = http.createServer(function (req, res) {
+    if (req.method === 'GET') {
+        fs.readFile('./views/calculator.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end();
+        });
+    } else {
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk;
+        })
+        req.on('end', () => {
+            const calculator = qs.parse(data);
+            fs.readFile('./views/calculator.html', 'utf8', function (err, datahtml) {
+                if (err) {
+                    console.log(err);
+                }
+                switch (calculator.cal) {
+                    case 'x':
+                        datahtml = datahtml.replace('{name}', userInfo.name);
+                }
+                
+                datahtml = datahtml.replace('{email}', userInfo.email);
+                datahtml = datahtml.replace('{password}', userInfo.password);
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(datahtml);
+                return res.end();
+            });
+        })
+        req.on('error', () => {
+            console.log('error')
+        })
+    }
+});
+
+server.listen(8080, function () {
+    console.log('server running at localhost:8080 ')
+});
---
+++ .history/BT_xayDungUngDungMayTinh/server_20221214111455.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const qs = require('qs')
+
+const server = http.createServer(function (req, res) {
+    if (req.method === 'GET') {
+        fs.readFile('./views/calculator.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end();
+        });
+    } else {
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk;
+        })
+        req.on('end', () => {
+            const calculator = qs.parse(data);
+            fs.readFile('./views/calculator.html', 'utf8', function (err, datahtml) {
+                if (err) {
+                    console.log(err);
+                }
+                switch (calculator.cal) {
+                    case 'x':
+                        datahtml = datahtml.replace('{result}', (calculator.a * calculator.b));
+                        break;
+                    case '/':
+                        datahtml = datahtml.replace('{result}', (calculator.a / calculator.b));
+                        break;
+                    case '+':
+                        datahtml = datahtml.replace('{result}', (calculator.a + calculator.b));
+                        break;
+                    case
+                }
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(datahtml);
+                return res.end();
+            });
+        })
+        req.on('error', () => {
+            console.log('error')
+        })
+    }
+});
+
+server.listen(8080, function () {
+    console.log('server running at localhost:8080 ')
+});
